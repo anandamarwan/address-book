a729159a0bf20c086d33c4ccda8eaa8dbf715d6c
Ananda Marwanaya Putra <anandamarwan20@gmail.com>
2024-05-08 19:44:40 -0700

    create initial file

---
+++ index.js
@@ -0,0 +1,23 @@
+const contacts = [
+  {
+    id: 1,
+    fullName: "Ananda Marwan",
+    age: 24,
+    phone: "+62 813 7103 1191",
+  },
+  {
+    id: 2,
+    fullName: "Salsabila",
+    age: 22,
+    phone: "+62 813 7103 1431",
+  },
+  {
+    id: 3,
+    fullName: "Amroati Soleha",
+    age: 40,
+    phone: "+62 813 7103 1531",
+  },
+];
+
+console.log(contacts);
+console.log(contacts[0]);
