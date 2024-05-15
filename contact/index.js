3c1dad77367ba617e6c28d5df8bec97a97b7851f
Ananda Marwanaya Putra <anandamarwan20@gmail.com>
2024-05-14 21:57:10 -0700

    show object in DOM

--- contact/index.js
+++ contact/index.js
@@ -1,21 +1,62 @@
-// Retrieve the array from local storage
-var storedArray = localStorage.getItem("address-book");
+// // Retrieve the object from local storage
+// const storedObject = localStorage.getItem("address-book");
 
-// Check if the array exists in local storage
-if (storedArray) {
-  // Parse the stored JSON string into an array of objects
-  var parsedArray = JSON.parse(storedArray);
+// // Check if the object exists in local storage
+// if (storedObject) {
+//   // Parse the stored JSON string into an object
+//   const parsedObject = JSON.parse(storedObject);
 
-  // Now you can use the parsedArray
-  console.log(parsedArray);
+//   // Now you can use the parsedObject
+//   console.log(parsedObject);
+// } else {
+//   console.log("No object found in local storage with the specified key.");
+// }
 
-  // Accessing individual objects in the array
-  parsedArray.forEach(function (obj) {
-    console.log("Full Name " + ":", obj.fullName);
-    console.log("Email " + ":", obj.email);
-    console.log("Phone " + ":", obj.phone);
-    console.log("Age " + ":", obj.age);
-  });
-} else {
-  console.log("No array found in local storage with the specified key.");
+// const contactItems = storedObject.join("");
+// parsedObject.innerHTML = contactItems;
+
+// Function to display data by ID
+function displayDataById(id) {
+  // Retrieve the array from local storage
+  var storedArray = localStorage.getItem("address-book");
+
+  // Check if the array exists in local storage
+  if (storedArray) {
+    // Parse the stored JSON string into an array of objects
+    var parsedArray = JSON.parse(storedArray);
+
+    // Find the object with the specified ID
+    var obj = parsedArray.find((item) => item.id === id);
+
+    // Get the container element where you want to display the data
+    var container = document.getElementById("data-container");
+
+    // Clear the container
+    container.innerHTML = "";
+
+    if (obj) {
+      var div = document.createElement("div");
+
+      div.innerHTML =
+        "Full Name: " +
+        obj.fullName +
+        "<br>" +
+        "Email: " +
+        obj.email +
+        "<br>" +
+        "Phone: " +
+        obj.phone +
+        "<br>" +
+        "Age: " +
+        obj.age;
+
+      container.appendChild(div);
+    } else {
+      container.innerHTML = "No data found with the specified ID.";
+    }
+  } else {
+    console.log("No array found in local storage with the specified key.");
+  }
 }
+
+displayDataById(4);
